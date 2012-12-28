2e62ab709e14461d353b3f90bd22d1a87a92a3f9
erik <erik168@163.com>
2012-12-28 16:55:37 +0800

    add object test

--- loader/test/src/deepDependency/level3.js
+++ loader/test/src/deepDependency/level3.js
@@ -3,16 +3,15 @@ define(
     [ 
         'deepDependency/level4'
     ], 
-    function ( level4 ) {
-        return {
-            name: 'deepDependency/level3',
-            check: function () {
-                var valid = 
-                    level4.name == 'deepDependency/level4'
-                    && level4.check();
-                    
-                return valid;
-            }
-        };
+    {
+        name: 'deepDependency/level3',
+        check: function () {
+            var level4 = require( 'deepDependency/level4' );
+            var valid = 
+                level4.name == 'deepDependency/level4'
+                && level4.check();
+                
+            return valid;
+        }
     }
 );
---
+++ loader/test/src/objectFactory/index.js
@@ -0,0 +1,3 @@
+define( 'objectFactory/index', {
+    name: 'objectFactory/index'
+} );
